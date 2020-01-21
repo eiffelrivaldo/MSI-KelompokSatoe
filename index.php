a9d0dd0a8267bc1e6c8bcfe9a9cf29ce7823747c
elyt0 <33003705+elyt0@users.noreply.github.com>
2020-01-21 14:26:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,62 +33,52 @@ $sessionKey= $lsJSONRPCClient->get_session_key( LS_USER, LS_PASSWORD );
 
 // receive surveys list current user can read
 $groups = $lsJSONRPCClient->list_surveys( $sessionKey );
-//print_r($groups, null );
-
-//Export token response in a survey.
-//$response = $lsJSONRPCClient->export_responses($sessionKey,$survey_id,'csv', null,'complete', 'short',$aFields = null);
-//var_dump($surveys);
 
 
-//respon jadi
+//API list_participants
 $response = $lsJSONRPCClient->list_participants($sessionKey,$survey_id, 0, 1000,true, true, $aConditions = array());
-//var_dump($surveys);
-
 
 //loop cek array
 if(is_array($response)){
-    echo json_encode($response);
+//    echo json_encode($response);
     
-    // print_r(base64_decode($idnya), null );
-    // echo($idnya);
 } else {
     print_r(base64_decode($response), null );
 }
 
-//kira2 array
+
+
 
 function build_table($response){
-    // start table
+    // membuat tabel
     $html = '<table>';
-    // header row
+    // mengisi header tabel
     $html .= '<tr>';
     foreach($response[0] as $key=>$value){
             $html .= '<th>' . htmlspecialchars($key) . '</th>';
         }
     $html .= '</tr>';
 
-    // data rows
-    foreach( $response as $key=>$value){
+    for ($i=0; $i < sizeof($response); $i++) { 
         $html .= '<tr>';
-        foreach($value as $key2=>$value2){
-            $html .= '<td>' . htmlspecialchars($value2) . '</td>';
-        }
+        //menampilkan isi tid
+        $html .= '<td>' . $response[$i]["tid"]  . '</td>';
+        //menampilkan isi token
+        $html .= '<td>' . $response[$i]["token"]  . '</td>';
+        //menampilkan isi participant_info
+        $html .= '<td>' . $response[$i]["participant_info"]["firstname"]  . '</td>';
         $html .= '</tr>';
     }
 
-    // finish table and return it
+    // selesai membuat tabel
 
     $html .= '</table>';
     return $html;
 }
 
-// $array = array(
-//     array('first'=>'tom', 'last'=>'smith', 'email'=>'tom@example.org', 'company'=>'example ltd'),
-//     array('first'=>'hugh', 'last'=>'blogs', 'email'=>'hugh@example.org', 'company'=>'example ltd'),
-//     array('first'=>'steph', 'last'=>'brown', 'email'=>'steph@example.org', 'company'=>'example ltd')
-// );
 
 echo build_table($response);
+
 //~ release the session key
 $lsJSONRPCClient->release_session_key( $sessionKey );
  ?>
